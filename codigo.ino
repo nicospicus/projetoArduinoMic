b38ff3eecac90b4bdedad6be1944d623c9e998fb
nicospicus <nicospicus@hotmail.com>
2016-07-09 00:49:07 -0300

    Configuração do Timer 1
    Código inicial do Timer 1. Ele fica operando no modo CTC (ou seja, ele vai contar até o valor em OCR1A e quando chegar nesse valor, zerar -- gerando a interrupção). Código comentado com os "cálculos" utilizados pra encontrar o valor de OCR1A.
    -- Testado no Proteus, 100% funcional. Caso queiram testar, só colocar o arduino e um terminal serial.
    -- O Print a cada adição dos segundos fica temporário ali, para vermos os segundos correndo.
    -- Vai dar errado ao chegar no 60, pois ele dá um print ao adicionar os segundos. Como a gente não vai dar esse print na realidade, esse problema não vai acontecer.
    -- Temos que verificar se não vai ser mais fácil separar os segundos em unidades, dezenas, e o mesmo para minutos e horas.
    Obs: os comentários estão bem completos, podemos retirar depois conforme formos desenvolvendo o documento, deixando aqui um pouco mais sucinto.

--- codigo.ino
+++ codigo.ino
@@ -1,4 +1,37 @@
+
+// DEFINIÇÃO DE VARIÁVEIS DO PROJETO.
+int segundos = 0;
+int minutos = 0;
+int horas = 0;
+
 void setup() {
+
+  Serial.begin(9600);
+
+  // Configuração do Timer 1 (de 16 bits) para contagem dos segundos.
+
+  // Inicialmente, coloca valor zero nos registradores do Timer 1.
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TIMSK1 = 0;
+  
+  // Configura o Prescaler para frequência de (16MHz/1024) = 15625Hz.
+  TCCR1B |= ( 1 << CS12 ) | ( 0 << CS11 ) | ( 1 << CS10 );
+
+  // Zera o valor inicial do Timer.
+  TCNT1 = 0;
+
+  // O valor máximo do counter será o número de ticks do pre-scaler para gerar 1s.
+  // Com frequência de 15625Hz (16Mhz/1024), serão necessários 15625 ticks para obter 1s.
+  OCR1A = 15625;
+
+  // Define o modo do Timer 1 como CTC (Clear Timer on Compare) com OCR1A.
+  TCCR1A |= ( 0 << WGM11 ) | ( 0 << WGM10 );
+  TCCR1B |= ( 0 << WGM13 ) | ( 1 << WGM12 );
+
+  // Habilita a interrupção ativada quando o valor do timer se torna igual ao valor do OCR1A.
+  TIMSK1 |= ( 1 << OCIE1A );
+
   
   /*  Definição dos registradores
    *  
@@ -10,30 +43,42 @@ void setup() {
 
 }
 
-ISR(//TIMER1_COMPA_vect){
-  // Inicializa OCR1A com (tempo_overflow - tempo_inicial) = (ciclos necessarios para 1s)
-  /* 
-   *  Nessa interrupção é legal só alterar um booleano, pra executar poucas ações.
-   */
-   
-  // booleano que indica a passagem de 1 segundo = true.
+ISR(TIMER1_COMPA_vect){
+  // Interrupção executada toda vez que TCTN1 = OCR1A. Ou seja, conforme as configurações, quando ele atinge 1s.
+  segundos = segundos + 1;
+
+  // Print temporário a cada alteração de segundos, para verificar a validade da implementação.
+  Serial.println( String(horas) + ':' + String(minutos) + ':' + String(segundos) );
 }
 
-ISR(//interrupção de leitura de serial){
+//ISR(//interrupção de leitura de serial){
   // Tem que ver como funciona, para que serve a interrupção.
 
   // O serial servirá para ler alterações do tempo do alarme e hora atual.
   // PESQUISAR SOBRE ISSO.
-}
+//}
 
-ISR(//interrupção externa 1 e 2){
+//ISR(interrupção externa 1 e 2){
   // Ativa booleanos que indicam que os botões foram pressionados.
 
   // Só ativa o booleano quando o botão for solto (FALLING EDGE) para evitar que o sistema siga infinitamente por alterações.
-}
+//}
 
 void loop() {
-  
+  // Verificação de troca entre segundos/minutos/horas.
+  if (segundos >= 60){
+    segundos = 0;
+    minutos = minutos + 1;
+
+    if (minutos >= 60){
+      minutos = 0;
+      horas = horas + 1;
+
+      if (horas >= 24){
+        horas = 0;
+      }
+    }
+  }
   /*
    *  -- quando a interrupção for executada, soma 1 no segundo
    * if (booleano de passagem de 1 segundo = true){
