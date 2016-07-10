bfa9aac0f2e4ce99d078cf0987cf4166767bab67
gabrielpra1 <gabrielpra1@gmail.com>
2016-07-10 17:25:34 -0300

    Update codigo.ino
    Pequenas mudancas.
    Adicionei a estrutura para implementarmos a questao da bateria.
    
    Acho que tudo que falta ser feito depende de hardware agora: potenciometro, displays, leds, alarmes...

--- codigo.ino
+++ codigo.ino
@@ -12,50 +12,41 @@ volatile boolean leu_string = false;
 unsigned char buffer_leitura[50];
 unsigned int buffer_index = 0;
 
+unsigned int bateria = 100;
+
 const char mensagem_erro[] = "Comando invalido!";
 
-void setup() {
-
-  // Configuração do USART0 em modo Assíncrono para Comunicação Serial.
-  // Para definir o BAUD Rate como 9600, calcula-se [freq/(16*BAUD)] - 1
-  UBRR0 = 103;
-
+void setup() 
+{
+  //=========== Configuração do USART0 em modo Assíncrono para Comunicação Serial. ===========
+  UBRR0 = 103; // Para definir o BAUD Rate como 9600, calcula-se UBR0 = [freq/(16*BAUD)] - 1
   UCSR0A = 0b00000000;
   // Habilita a interrupção de fim de leitura de dados e habilita a recepção de dados.
-  UCSR0B = 0b00000000 | ( 1 << RXCIE0 ) | ( 1 << RXEN0 ) | ( 1 << TXEN0 );  // DESABILITAR O TXEN AO FINAL, USADO EXPERIMENTALMENTE.
+  UCSR0B = 0b00000000 | ( 1 << RXCIE0 ) | ( 1 << RXEN0 ) | ( 1 << TXEN0 );
   // Define: operação em modo assíncrono; sem paridade; 1 stop bit; 8 bits; 1 stop bit
   UCSR0C = 0b00000000 | ( 1 << UCSZ01 ) | ( 1 << UCSZ00 );
+  
 
-  // Configuração do Timer 1 (de 16 bits) para contagem dos segundos.
-
+  //=========== Configuração do Timer 1 (de 16 bits) para contagem dos segundos. ===========
   // Configura o Prescaler para frequência de (16MHz/1024) = 15625Hz.
   TCCR1B = 0b00000000 | ( 1 << CS12 ) | ( 0 << CS11 ) | ( 1 << CS10 );
-
-  // Zera o valor inicial do Timer.
-  TCNT1 = 0;
-
+  TCNT1 = 0;  // Zera o valor inicial do Timer.
+  
   // O valor máximo do counter será o número de ticks do pre-scaler para gerar 1s.
-  // Com frequência de 15625Hz (16Mhz/1024), serão necessários 15624 ticks para obter 1s.
-  OCR1A = 15624;   // No modo CTC, sempre que o tempo desejado é atingido, ele se reseta.
-                   //Como o reset leva um ciclo de clock para ser concluído, temos 15625-1=15624
-
+  // Com frequência de 15625Hz, serão necessários 15625 ticks para obter 1s.
+  // Mas, no modo CTC, sempre que o tempo desejado é atingido, ele se reseta.
+  // Como o reset leva um ciclo de clock para ser concluído, temos 15625-1=15624
+  OCR1A = 15624; 
+  
   // Define o modo do Timer 1 como CTC (Clear Timer on Compare) com OCR1A.
   TCCR1A = 0b00000000 | ( 0 << WGM11 ) | ( 0 << WGM10 );
   TCCR1B |= ( 0 << WGM13 ) | ( 1 << WGM12 );
 
   // Habilita a interrupção ativada quando o valor do timer se torna igual ao valor do OCR1A.
   TIMSK1 = 0b00000000 | ( 1 << OCIE1A );
-
-  /*  Definição dos registradores
-   *  TIMER A
-   *  Definido para gerar interrupção a cada 1 segundo (configurar contador e pre-scaler).
-   *  
-   *  CONFIGURAR INTERRUPÇÕES EXTERNAS, SERIAL, ETC.
-   */
    
    
-  //Configuração das interrrupções externas:
- 
+  //=========== Configuração das interrrupções externas: =========== 
   //Primeiro botão:
   EIMSK = (1 << INT0);
   EICRA = (1 << ISC01) | (0 << ISC00);
@@ -66,7 +57,6 @@ void setup() {
   EIMSK = (1 << INT1);
   EICRA = (1 << ISC11) | (0 << ISC10);
   //Da mesma forma, acionamento em 'FALLING EDGE'
-
 }
 
 // Interrupção executada toda vez que TCTN1 = OCR1A. Ou seja, conforme as configurações, quando ele atinge 1s.
@@ -85,30 +75,31 @@ ISR(USART_RX_vect)
   }
 }
 
-//Serial print
+//Imprime no serial.
 void imprimir(char[] texto)
 {
-	int i = 0;
-	while(texto[i] != '\0')
+    int i = 0;
+    while(texto[i] != '\0')
     {
-		while( !(UCSR0A & ( 1 << TXC0 )) ); //Espera estar livre pra escrever
+	while( !(UCSR0A & ( 1 << TXC0 )) ); //Espera estar livre pra escrever
         UDR0 = buffer_leitura[i++];
     }
 }
 
-//Interrupções Externas  (Botões 1 e 2 do relógio)
 
- ISR(INT0_vect){
-  //alarme_ativado = true;
-  // Ativa booleano que indica que o botão foi pressionado.
- }
+//Interrupcao para o botao 1, que ativa/desativa alarme.
+ISR(INT0_vect)
+{
+  alarme_ativado = !alarme_ativado;
+  //Falta ligar alguma coisa que mostre o estado do alarme.
+}
  
-  ISR(INT1_vect){
-  //alarme_tocando = true;
-  // Ativa booleano que indica que o botão foi pressionado.
- }
- 	
- }
+//Interrupcao para o botao 2, que faz com que o alarme pare de tocar.
+ISR(INT1_vect)
+{
+  alarme_tocando = false;
+}
+ 
 
 void loop() {
   // Verificação de troca entre segundos/minutos/horas.
@@ -116,6 +107,9 @@ void loop() {
   {
     segundos = 0;
     minutos = minutos + 1;
+    bateria--; //A cada minuto, perde 1% de bateria.
+    //Aqui, vamos usar o valor lido do potenciometro para adicionar um valor
+    //entre 0 e 3 (ou outros valores) à bateria.
 
     if (minutos >= 60)
     {
@@ -142,13 +136,13 @@ void loop() {
    */
    if(alarme_ativado)
    {
-	   if(horas == alarme_horas && minutos == alarme_minutos)
-	   {
-		   //Toca alarme
-		   //Como faremos para tocar o alarme?
-		   //Será que conseguimos gerar algum som pelo computador, ou será só visual?
-		   alarme_tocando = true;
-	   }
+	if(horas == alarme_horas && minutos == alarme_minutos)
+	{
+	     //Toca alarme
+	     //Como faremos para tocar o alarme?
+	     //Será que conseguimos gerar algum som pelo computador, ou será só visual?
+	     alarme_tocando = true;
+	}
    }
   
    if(alarme_tocando)
@@ -161,47 +155,36 @@ void loop() {
    // Se lida uma string por completo, uma linha inteira estará presente no buffer_leitura.
   if(leu_string)
    {
-     // DEFINIR OS NOVOS COMANDOS AO TERMINAR LEITURA AQUI. DEFINIR PROTOCOLO DO TIPO 'a11:20' ativa o alarme ou 'h20:20' muda a hora.
-    
-     // Por ora, coloquei Print dos caracteres inseridos no buffer.
-     
-	 int i = 0;
+    // Por ora, coloquei Print dos caracteres inseridos no buffer.
+    i = 0;
      while(buffer_leitura[i] != '\0')
      {
-		while( !(UCSR0A & ( 1 << TXC0 )) );
+	while( !(UCSR0A & ( 1 << TXC0 )) );
         UDR0 = buffer_leitura[i++];
      }
 	// Aqui termina a parte temporária.
 	
  
-	 //Configura hora.
-	 if(buffer_leitura[0] == 'H' || buffer_leitura[0] == "h")
-	 {
-		 //Verifica se o comando faz sentido
-		if((buffer_leitura[1]*10 + buffer_leitura[2] >= 0 || buffer_leitura[1]*10 + buffer_leitura[2] < 24)
-			&& (buffer_leitura[4]*10 + buffer_leitura[5] >= 0 || buffer_leitura[4]*10 + buffer_leitura[5] < 60))
+	//Verifica se o comando faz sentido
+	if((buffer_leitura[1]*10 + buffer_leitura[2] >= 0 || buffer_leitura[1]*10 + buffer_leitura[2] < 24)
+		&& (buffer_leitura[4]*10 + buffer_leitura[5] >= 0 || buffer_leitura[4]*10 + buffer_leitura[5] < 60))
+	{
+		//Configura hora
+		if(buffer_leitura[0] == 'H' || buffer_leitura[0] == "h")
 		{
 			horas = buffer_leitura[1]*10 + buffer_leitura[2];
 			minutos = buffer_leitura[4]*10 + buffer_leitura[5];
-		}
-		else
-			imprimir(mensagem_erro);
-	 }
-	 
-	 //Configura alarme
-	 else if(buffer_leitura[0] == 'A' || buffer_leitura[0] == 'a')
-	 {
-		//Verifica se o comando faz sentido
-		if((buffer_leitura[1]*10 + buffer_leitura[2] >= 0 || buffer_leitura[1]*10 + buffer_leitura[2] < 24)
-			&& (buffer_leitura[4]*10 + buffer_leitura[5] >= 0 || buffer_leitura[4]*10 + buffer_leitura[5] < 60))
+		}	 
+		//Configura alarme
+		else if(buffer_leitura[0] == 'A' || buffer_leitura[0] == 'a')
 		{
 			alarme_horas = buffer_leitura[1]*10 + buffer_leitura[2];
 			alarme_minutos = buffer_leitura[4]*10 + buffer_leitura[5];
-		}
-		else
-			imprimir(mensagem_erro);
-	 }
-	 else //Se o comando for invalido.
+		}	
+		else //Se o comando for invalido
+			imprimit(mensagem_erro);
+	}
+	else //Se o comando for invalido.
 		imprimir(mensagem_erro);
 
      // Esvazia o que está presente no buffer_leitura e retorna as variáveis booleanas aos valores originais.
@@ -210,11 +193,6 @@ void loop() {
      buffer_leitura[0] = '\0';
    }
    
-   
-  /*
-   * Ver se o botão foi pressionado para ativar/desativar alarme ou parar de tocar.
-   * Dois botões: alarme e soneca.
-   */
 
   /*
    * PENSAR SOBRE ALGO PARA CONVERSÃO A/D!
@@ -227,5 +205,5 @@ void loop() {
      gerar valores aleatório a cada hora, algo assim.
      
      O que acham? Só uma ideia mesmo.
-   */
+   */  
 }
